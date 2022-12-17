ae1cd01ce326e5d6113c1e8219afc58c34de445f
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-12-17 14:53:28 +0330

    All

---
+++ Day 001 - 010/Day 1/Analog Clock/main.js
@@ -0,0 +1,14 @@
+const hour = document.querySelector ("#hour");
+const minute = document.querySelector ("#minute");
+const second = document.querySelector ("#second");
+
+setInterval(function(){
+	let day = new Date();
+	let h = day.getHours() * 30; // 360/12=30
+	let m = day.getMinutes() * 6; // 360/60=6
+	let s = day.getSeconds() * 6; // 360/60=6
+
+	hour.style.transform = `rotateZ(${h}deg)`;
+	minute.style.transform = `rotateZ(${m}deg)`;
+	second.style.transform = `rotateZ(${s}deg)`;
+}, 1000);
