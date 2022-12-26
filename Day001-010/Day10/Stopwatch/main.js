76de13b4fe5d99d59dc9f646939cfcb433b411e0
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-12-26 14:54:56 +0330

    All

---
+++ Day001-010/Day10/Stopwatch/main.js
@@ -0,0 +1,44 @@
+const sec = document.querySelector(".sec");
+const millisec = document.querySelector(".millisec");
+const controller = document.querySelectorAll(".controller span")
+
+let second = 00;
+let millisecond = 00;
+let myTimer;
+
+controller.forEach(function(item){
+	item.addEventListener("click",function(item){
+		if (item.target.classList.contains("start")) {
+			myTimer = setInterval(update,10);
+		} else if (item.target.classList.contains("stop")) {
+			clearInterval(myTimer);
+		} else {
+			clearInterval(myTimer);
+			second = "00";
+			millisecond = "00";
+			sec.innerText=second;
+			millisec.innerText=millisecond;
+		}
+	})
+})
+
+
+function update() {
+	millisecond++;
+	if (millisecond > 99) {
+		second++;
+		if (second < 10) {
+			second = `0${second}`;
+		}
+		millisecond = 0;
+		console.log("sec:"+second);
+	}
+
+	if (millisecond < 10) {
+		millisecond = `0${millisecond}`;
+		console.log(millisecond);
+	}
+
+	sec.innerText=second;
+	millisec.innerText=millisecond;
+}
