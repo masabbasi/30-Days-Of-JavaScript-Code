b365a489762e71729ccf512892913221d9e1b682
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-12-27 22:12:13 +0330

    All

--- Day011-020/Day11/Eyes/main.js
+++ Day011-020/Day11/Eyes/main.js
@@ -7,12 +7,8 @@ function eyeball () {
 		eyes.forEach(function(eye){
 		let x = eye.getBoundingClientRect().left + eye.clientWidth/2;
 		let y = eye.getBoundingClientRect().top + eye.clientHeight/2;
-		console.log(x);
-		console.log(y);
 		let radian = Math.atan2(event.pageX - x, event.pageY - y);
-		console.log(radian);
 		let rotate = radian * (180/Math.PI)* -1 + 270;
-		console.log(rotate);
 		eye.style.transform = `rotate(${rotate}deg)`;
 	})
 }
