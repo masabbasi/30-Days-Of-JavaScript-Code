f1e752dd5a02e578ee966d3f6d472a08bed058fc
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-12-28 15:40:33 +0330

    All

--- Day011-020/Day12/Accordion/main.js
+++ Day011-020/Day12/Accordion/main.js
@@ -2,9 +2,25 @@ const oC = document.querySelectorAll(".accordion__oc");
 const accordionHead = document.querySelectorAll(".accordion__head");
 const accordionBody = document.querySelectorAll(".accordion__body");
 
-for (let i = 0; i < accordionHead.length; i++) {
-  accordionHead[i].addEventListener("click", function () {
-    oC[i].classList.toggle("accordion__plus");
-    accordionBody[i].classList.toggle("accordion__body--open");
+accordionHead.forEach((headItem, headIndex) => {
+  headItem.addEventListener("click", () => {
+    accordionBody.forEach((bodyItem, bodyIndex) => {
+      if (headIndex === bodyIndex) {
+        bodyItem.classList.toggle("accordion__body--open");
+        oC[headIndex].classList.toggle("accordion__plus");
+      }
+    });
   });
-}
+});
+
+
+// const oC = document.querySelectorAll(".accordion__oc");
+// const accordionHead = document.querySelectorAll(".accordion__head");
+// const accordionBody = document.querySelectorAll(".accordion__body");
+
+// for (let i = 0; i < accordionHead.length; i++) {
+//   accordionHead[i].addEventListener("click", function () {
+//     oC[i].classList.toggle("accordion__plus");
+//     accordionBody[i].classList.toggle("accordion__body--open");
+//   });
+// }
