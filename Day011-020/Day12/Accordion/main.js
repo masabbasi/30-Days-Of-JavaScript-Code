e743ac037f57b2f245a6617c8e22b94ff7954f68
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-12-28 15:21:18 +0330

    All

--- Day011-020/Day12/Accordion/main.js
+++ Day011-020/Day12/Accordion/main.js
@@ -1,10 +1,10 @@
 const oC = document.querySelectorAll(".accordion__oc");
+const accordionHead = document.querySelectorAll(".accordion__head");
 const accordionBody = document.querySelectorAll(".accordion__body");
 
-
-for (let i=0;i<oC.length;i++) {
-	oC[i].addEventListener("click",function(){
-		oC[i].classList.toggle("accordion__plus");
-		accordionBody[i].classList.toggle("accordion__body--open");
-	})
+for (let i = 0; i < accordionHead.length; i++) {
+  accordionHead[i].addEventListener("click", function () {
+    oC[i].classList.toggle("accordion__plus");
+    accordionBody[i].classList.toggle("accordion__body--open");
+  });
 }
