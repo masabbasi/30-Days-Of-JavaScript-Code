e9987fe4898c51348ee142283e62c2459a2f192e
MasAbbasi <masoud.abbasi.71@gmail.com>
2023-01-02 23:08:24 +0330

    All

--- Day011-020/Day17/Bell-Notification/main.js
+++ Day011-020/Day17/Bell-Notification/main.js
@@ -1,14 +1,20 @@
 const btn = document.querySelector(".btn");
 const notify__counter = document.querySelector(".notify__counter");
+const animation = document.querySelector(".notify img");
 let counter = 0;
 
-btn.addEventListener("click",function(){
+btn.addEventListener("click", function () {
 	if (notify__counter.classList.contains("counter")) {
-		counter++
-		document.querySelector(".counter").innerText = counter;
-	} else {
+		animation.classList.add("notify__alert");
+    counter++;
+    document.querySelector(".counter").innerText = counter;
+  } else {
+		animation.classList.add("notify__alert");
 		notify__counter.classList.toggle("counter");
-		counter++
-		document.querySelector(".counter").innerText = counter;
-	}
-})
+    counter++;
+    document.querySelector(".counter").innerText = counter;
+  }
+	animation.addEventListener("animationend", () => {
+		animation.classList.remove("notify__alert");
+	})
+});
