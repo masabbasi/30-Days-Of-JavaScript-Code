831bce839973eaef916cf6a12984a9af5e84d309
MasAbbasi <masoud.abbasi.71@gmail.com>
2023-01-04 12:50:48 +0330

    All

---
+++ Day011-020/Day19/Timeline/main.js
@@ -0,0 +1,72 @@
+const btns = document.querySelectorAll(".btn");
+const circles = document.querySelectorAll(".circle");
+const timeLine = document.querySelector(".timeline");
+let timeLineWidth = 0;
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    if (btn.classList.contains("btn__next")) {
+      borojolo();
+    } else {
+      biaaghab();
+    }
+  });
+});
+
+function borojolo() {
+  switch (timeLineWidth) {
+    case 0: {
+      timeLineWidth = 25;
+      circles[1].style.borderColor = "rgb(49, 226, 49)";
+      break;
+    }
+    case 25: {
+      timeLineWidth = 50;
+      circles[2].style.borderColor = "rgb(49, 226, 49)";
+      break;
+    }
+    case 50: {
+      timeLineWidth = 75;
+      circles[3].style.borderColor = "rgb(49, 226, 49)";
+      break;
+    }
+    case 75: {
+      timeLineWidth = 100;
+      circles[4].style.borderColor = "rgb(49, 226, 49)";
+      break;
+    }
+    case 100: {
+      break;
+    }
+  }
+  timeLine.style.width = `${timeLineWidth}%`;
+}
+
+function biaaghab() {
+  switch (timeLineWidth) {
+    case 100: {
+      timeLineWidth = 75;
+      circles[4].style.borderColor = "rgb(78, 76, 76)";
+      break;
+    }
+    case 75: {
+      timeLineWidth = 50;
+      circles[3].style.borderColor = "rgb(78, 76, 76)";
+      break;
+    }
+    case 50: {
+      timeLineWidth = 25;
+      circles[2].style.borderColor = "rgb(78, 76, 76)";
+      break;
+    }
+    case 25: {
+      timeLineWidth = 0;
+      circles[1].style.borderColor = "rgb(78, 76, 76)";
+      break;
+    }
+    case 2: {
+      break;
+    }
+  }
+  timeLine.style.width = `${timeLineWidth}%`;
+}
