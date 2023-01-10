f6e497762d9e4a96618cad5307e22d26656a3575
MasAbbasi <masoud.abbasi.71@gmail.com>
2023-01-10 14:01:27 +0330

    All

--- Day021-030/Day23/Random-Picker-Visualizer/main.js
+++ Day021-030/Day23/Random-Picker-Visualizer/main.js
@@ -4,18 +4,15 @@ const list = document.querySelector(".tags ul");
 textArea.focus();
 
 textArea.addEventListener("keyup", function (e) {
-	createTag(e.target.value)
+			createTag(e.target.value);
 });
-
-
 
 function createTag(all){
   let tags = all.split('.').filter(tag => tag.trim() !== '').map(tag => tag.trim());
 	list.innerHTML = '';
 	tags.forEach(function(tag){
 		let li = document.createElement("li");
-		// li.innerText=`#${tag}`;
-		li.innerText=`#${tag.replace(" ","_")}`;
+		li.innerText=`#${tag.replace(/\s{1,}/g, '_')}`;
 		list.appendChild(li);
 	})
 }
