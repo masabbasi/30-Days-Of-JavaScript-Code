8db181d889e371d7ab14d05138346f010e266f2d
MasAbbasi <masoud.abbasi.71@gmail.com>
2023-01-10 14:48:32 +0330

    All

--- Day021-030/Day23/Random-Picker-Visualizer/main.js
+++ Day021-030/Day23/Random-Picker-Visualizer/main.js
@@ -16,3 +16,14 @@ function createTag(all){
 		list.appendChild(li);
 	})
 }
+
+//			 Tags separator : Two Spaces ("  ")
+// function createTag(all){
+//   let tags = all.split(/\s{2,}/g).filter(tag => tag.trim() !== '');
+// 	list.innerHTML = '';
+// 	tags.forEach(function(tag){
+// 		let li = document.createElement("li");
+// 		li.innerText=`#${tag.replace(/\s{1,}/g, '_')}`;
+// 		list.appendChild(li);
+// 	})
+// }
