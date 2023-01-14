836d4e502bc1e03e88ca2e2dff4f0227fb1ea227
MasAbbasi <masoud.abbasi.71@gmail.com>
2023-01-14 13:49:04 +0330

    All

--- Day021-030/Day27/Fluid-Image-Pop-Up/main.js
+++ Day021-030/Day27/Fluid-Image-Pop-Up/main.js
@@ -1,22 +1,18 @@
 const images = document.querySelectorAll(".other__img img");
 const imgShowBox = document.querySelector(".show__img");
 const imgShow = document.querySelector(".show__img img");
- 
-images.forEach((image)=>{
-image.addEventListener("click",()=>{
-	imgShowBox.style.opacity="1";
-	imgShowBox.style.pointerEvents="all";
-	imgShow.src=`${image.src}`;
-	imgShow.style.transform= "translate(-50%, -50%) scale(1)";
-})
-})
 
-imgShowBox.addEventListener("click",(e)=>{
-	if (e.target.classList.contains("show__img")) {
-		imgShowBox.style.opacity="0";
-		imgShowBox.style.pointerEvents="none";
-		imgShow.src="";
-		imgShow.style.transform= "translate(-50%, -50%) scale(0.4)";
-		console.log("hi");
-	}
-})
+images.forEach((image) => {
+  image.addEventListener("click", () => {
+    imgShowBox.classList.add("active");
+    imgShow.src = `${image.src}`;
+  });
+});
+
+imgShowBox.addEventListener("click", (e) => {
+  if (e.target.classList.contains("active")) {
+		console.log("contains");
+    imgShowBox.classList.remove("active");
+    imgShow.src = "";
+  }
+});
