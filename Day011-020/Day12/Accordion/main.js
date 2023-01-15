bf14dccfd1bd209ef55863e8e4b31095abba5c26
MasAbbasi <masoud.abbasi.71@gmail.com>
2023-01-15 17:29:15 +0330

    All

--- Day011-020/Day12/Accordion/main.js
+++ Day011-020/Day12/Accordion/main.js
@@ -1,18 +1,46 @@
-const oC = document.querySelectorAll(".accordion__oc");
+// get Element Height
+const oC = document.querySelectorAll(".accordion__oc"); //oc : open close
 const accordionHead = document.querySelectorAll(".accordion__head");
 const accordionBody = document.querySelectorAll(".accordion__body");
+const allHeight = [];
+
+accordionBody.forEach(function (item) {
+  allHeight.push(item.offsetHeight);
+  item.style.height = "0px";
+});
 
 accordionHead.forEach((headItem, headIndex) => {
   headItem.addEventListener("click", () => {
     accordionBody.forEach((bodyItem, bodyIndex) => {
       if (headIndex === bodyIndex) {
-        bodyItem.classList.toggle("accordion__body--open");
+        if (bodyItem.style.height == "0px") {
+          bodyItem.style.height = `${allHeight[bodyIndex]}px`;
+        } else {
+          bodyItem.style.height = "0px";
+        }
         oC[headIndex].classList.toggle("accordion__plus");
       }
     });
   });
 });
 
+// Set max-height
+// const oC = document.querySelectorAll(".accordion__oc");
+// const accordionHead = document.querySelectorAll(".accordion__head");
+// const accordionBody = document.querySelectorAll(".accordion__body");
+
+// accordionHead.forEach((headItem, headIndex) => {
+//   headItem.addEventListener("click", () => {
+//     accordionBody.forEach((bodyItem, bodyIndex) => {
+//       if (headIndex === bodyIndex) {
+//         bodyItem.classList.toggle("accordion__body--open");
+//         oC[headIndex].classList.toggle("accordion__plus");
+//       }
+//     });
+//   });
+// });
+
+// another
 // const oC = document.querySelectorAll(".accordion__oc");
 // const accordionHead = document.querySelectorAll(".accordion__head");
 // const accordionBody = document.querySelectorAll(".accordion__body");
